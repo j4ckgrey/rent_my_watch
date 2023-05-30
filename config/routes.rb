95460d76b35b832a72a58f59a8c82dfea9b6a80d
jack grey <j4ckgrey@gmail.com>
2023-05-30 17:41:50 +0200

    dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+
   devise_for :users
   resources :watches do
     resources :rentals, only: [:new, :create]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "/dashboard", to: "pages#dashboard"
   # Defines the root path route ("/")
   # root "articles#index"
 end
