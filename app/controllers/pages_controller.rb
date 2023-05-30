8f4fb7ea440223d8d43505cfe54be57e8556f124
jack grey <j4ckgrey@gmail.com>
2023-05-30 16:51:04 +0200

    show page almost complete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def home
     @watches = Watch.all
   end
+
+  def dashboard
+    @user = current_user
+  end
 end
---
+++ db/migrate/20230530125448_add_description_to_watches.rb
@@ -0,0 +1,5 @@
+class AddDescriptionToWatches < ActiveRecord::Migration[7.0]
+  def change
+    add_column :watches, :description, :text
+  end
+end
