a207a1ece1f026460a868b47f5b5a9df0c4ba076
jack grey <j4ckgrey@gmail.com>
2023-05-31 10:43:52 +0200

    deleted all watches and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-Watch.create!(name: "Hulk", brand: "Rolex", model: "Submariner")
-Watch.create(name: "Moonwatch", brand: "Omega", model: "Speedmaster Professional")
-Watch.create(name: "President", brand: "Rolex", model: "Day-Date")
-Watch.create!(name: "Batmann", brand: "Rolex", model: "Submariner")
-Watch.create(name: "Apollo11", brand: "Omega", model: "Speedmaster Professional")
