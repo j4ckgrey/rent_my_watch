e09ced8642623a7894dc23bacea6e9ef42df57ca
Tim Schulze Sölde <tim@rotthege.com>
2023-06-01 11:17:16 +0200

    implemented the search bar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,27 @@ class PagesController < ApplicationController
   def home
     if current_user.nil?
       @watches = Watch.all
+      if params[:query].present?
+        sql_subquery = <<~SQL
+          watches.name @@ :query
+          OR watches.brand @@ :query
+          OR watches.model @@ :query
+          OR watches.description @@ :query
+        SQL
+        @watches = @watches.where(sql_subquery, query: params[:query])
+      end
     else
       @pick = []
       @watches = Watch.all
+      if params[:query].present?
+        sql_subquery = <<~SQL
+          watches.name @@ :query
+          OR watches.brand @@ :query
+          OR watches.model @@ :query
+          OR watches.description @@ :query
+        SQL
+        @watches = @watches.where(sql_subquery, query: params[:query])
+      end
       @watches.each do |watch|
         @pick << watch if watch.user_id != current_user.id
       end
