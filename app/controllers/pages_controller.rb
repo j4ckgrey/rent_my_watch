429435efefd527098728933be9f5ef086d8176fc
jack grey <j4ckgrey@gmail.com>
2023-06-01 16:04:21 +0200

    status implemeneted to Rentals

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,4 +8,9 @@ class PagesController < ApplicationController
     @mywatches = current_user.watches
     @myrentals = current_user.rentals
   end
+
+  def rental_requests
+    @user = current_user
+    @watches = @user.watches
+  end
 end
